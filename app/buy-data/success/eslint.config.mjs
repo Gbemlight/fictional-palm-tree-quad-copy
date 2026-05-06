62ff3b08df61f0ca1fd95e553ed2d63acd039307
Gbemlight <adeyanjusunday114@gmail.com>
2026-05-06 14:48:38 +0100

    update

---
+++ app/buy-data/success/eslint.config.mjs
@@ -0,0 +1,16 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+// mimic CommonJS __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  // Optionally, specify other base directories for plugins and configs
+  // resolvePluginsRelativeTo: __dirname,
+});
+
+// Use the FlatCompat utility to extend from existing configs
+export default [...compat.extends("next/core-web-vitals", "next/typescript")];
